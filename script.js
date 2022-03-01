387f0c820a9f333b61b6e9402aa4ad85a91b5798
Venator71 <b.johnhunter71@gmail.com>
2022-03-01 09:19:34 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+// Q 1
+console.log(55);
+
+// Q 2
+/* Numbers
+Strings
+Boolean
+Null
+Undefined
+*/
+
+// Q 3
+/*
++ add
+- subtract
+* multiply
+/ divide
+** exponents
+% modulus
+*/
+
+// Q 4
+
+console.log( 5 % 2);
+console.log(5 % 4);
+
+// Q 5
+
+console.log ('I\'M MARY POPPINS, Y\'ALL!');
+
+// Q 6
+console.log("Hawk" + " Eye");
+
+// Q 7
+console.log("John".length + "Hunter".length);
+
+// Q 8
+//console.log("Avengers: Endgame".length);
+console.log("Avengers: Endgame"[16]);
+
+// B 1
+console.log("\\");
+
+// B 2
+console.log( (5 ** 4) - 9);
+
+// B 3
+console.log("I".length + "Am".length % "Iron".length - "Man".length);
+
+
+// B 4
+console.log("CAP"[1]);
+console.log("VISION"[0]);
+console.log("OKOYE"[4]);
+console.log("NICK FURY"[0]);
+console.log("GROOT"[0]);
+console.log("DOCTOR STRANGE"[13]);
+console.log("THOR"[3]);
+console.log("STAR LORD"[0]);
+
+
+//B 5
+let m = 25;
+let c = 4;
+let u = m + c;
+console.log(u);
+
+
+
+
